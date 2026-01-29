a4d1798d154f216b50748904393e71fa86840f81
sabinMas <202682616+sabinMas@users.noreply.github.com>
2026-01-29 14:36:38 -0800

    add app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+// import the express module
+import express from 'express';
+
+// create an instance of express 
+const app = express();
+
+// define port
+const PORT = 3000;
+
+// define default route
+
+app.get('/', (req,res)=> {
+    // res.sendFile(`${import.meta.dirname}/views/home.html`);
+    res.send('welcom to our ice cream shop')
+});
+
+app.listen(PORT,()=> {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});

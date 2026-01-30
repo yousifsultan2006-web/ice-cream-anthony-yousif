ef2919b1187114ae3e93e72fcb39af7b3fde4bd6
YousifSultan <231142289+yousifsultan2006-web@users.noreply.github.com>
2026-01-29 16:03:04 -0800

    fixed some css to size down the image, added span id and classes to use for validations, fixed sourcing for linking both css and JS, added validation for Name

--- public/js/ice-cream.js
+++ public/js/ice-cream.js
@@ -1,1 +1,45 @@
 alert("hello Ice cream!");
+document.getElementById("ice-cream-form").onsubmit = () => {
+
+clearerrors();
+
+let isValid= true;
+
+
+
+
+//validate  name
+let firstName = document.getElementById("name").value.trim();
+if (!firstName) {
+    document.getElementById("name-error").style.display = "block";
+    isValid = false;
+
+}
+//validate email
+
+//validate cone type
+
+
+
+
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  
+  const flavor = document.getElementById("flavor").value;
+    
+
+});
+
+
+
+
+
+};
+
+function clearerrors() {
+    let errors = document.getElementsByClassName("error");
+    for (let error of errors) {
+        error.style.display = "none";
+    };
+}

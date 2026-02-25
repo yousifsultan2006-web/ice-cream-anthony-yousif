3820c3b186d2498a584d2162a308aff2869b7a28
YousifSultan <231142289+yousifsultan2006-web@users.noreply.github.com>
2026-02-25 05:14:55 -0800

    changed all html files to ejs, set view engine to ejs, replaced all old routes so they use ejs (res.render instead of res.sendFile), added admin and confirmation routes and pages (all ejs). linked admin view ejs with datatables for both script and css. fixed some naming mismatches with toppings and comments

--- app.js
+++ app.js
@@ -7,14 +7,44 @@ const app = express();
 // define port
 const PORT = 8000;
 
+//set up view engine
+app.set('view engine', 'ejs');
+
+//middleware to parse incoming request bodies
+app.use(express.urlencoded({extended: true}));
+
 app.use(express.static('public'));
+const orders = [];
 
 // define default route
 
 app.get('/', (req,res)=> {
-     res.sendFile(`${import.meta.dirname}/views/home.html`);
+     res.render('home');
    // res.send('welcom to our ice cream shop')
 });
+app.get('/admin', (req, res) => {
+  res.render('admin', { orders });
+});
+
+app.post('/submit-order', (req, res) => {
+
+  const order = {
+    name: req.body.name,
+    email: req.body.email,
+    flavor: req.body.flavor,
+    cone: req.body.cone,
+    toppings: req.body.toppings ? req.body.toppings : "none",
+    comments: req.body.comments,
+
+    timestamp: new Date()
+  };
+  orders.push(order);
+  res.render('confirmation', { order });
+
+
+
+});
+
 
 app.listen(PORT,()=> {
     console.log(`Server is running at http://localhost:${PORT}`);
